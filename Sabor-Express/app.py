1393337d9daf044dd99376e4c2ef580469f6225e
macabuster <macabuster@gmail.com>
2026-04-06 22:16:21 -0300

    Add close app function

--- Sabor-Express/app.py
+++ Sabor-Express/app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
 ╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮╱╭━━━╮
 ┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╯┃╱┃╭━╮┃
@@ -13,9 +15,12 @@ print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair \n')
 
-option_Choice = int(input('Escolha uma opção: ') )
+option_Choice = int(input('Escolha uma opção: '))
 
-
+def close_app():
+    os.system('clear')
+    print('Encerrando o programa')
+    
 if option_Choice == 1:
     print('Cadastrar restaurante')
 elif option_Choice == 2:
@@ -23,4 +28,4 @@ elif option_Choice == 2:
 elif option_Choice == 3:
     print('Ativar restaurante')
 else:
-    print('Encerrando o programa')
+   close_app()
