bc12c13e9ae60f78fdb0c6add0caccfa753d6897
macabuster <macabuster@gmail.com>
2026-04-08 00:35:34 -0300

    Add create and list functions

--- Sabor-Express/app.py
+++ Sabor-Express/app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurants = ['Pizza', 'Comet']
+
 def show_program_name():
     print("""
     ╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮╱╭━━━╮
@@ -17,18 +19,44 @@ def show_options():
     print('3. Ativar restaurante')
     print('4. Sair \n')
 
+def close_app():
+    os.system('clear')
+    print('Encerrando o programa')
+
+def return_main_menu():
+    input('\nDigite uma tecla para voltar ao menu principal: ')
+    main()
+
 def invalid_option():
     print('Opção inválida! \n')
-    input('Digite uma tecla para voltar ao menu principal')
+    input('\nDigite uma tecla para voltar ao menu principal: ')
     main()
+
+def create_restaurant():
+    os.system('clear')
+    print('Cadastrar restaurante: \n')
+    restaurant_name = input('Digite o nome do restaurante para que deseja cadastrar: ')
+    restaurants.append(restaurant_name)
+    print(f'Restaurante cadastrado com sucesso! \n [ {restaurant_name} ] ✅')
+
+    return_main_menu()
+
+def list_restaurants():
+    os.system('clear')
+    print('Restaurantes cadastrados: \n')
+
+    for restaurant in restaurants: 
+        print(f'- {restaurant} \n')
+   
+    return_main_menu()
 
 def select_options():
     try:
         option_Choice = int(input('Escolha uma opção: '))
         if option_Choice == 1:
-            print('Cadastrar restaurante')
+            create_restaurant()
         elif option_Choice == 2:
-            print('2. Listar restaurante')
+            list_restaurants()
         elif option_Choice == 3:
             print('Ativar restaurante')
         elif option_Choice == 4:
@@ -37,10 +65,6 @@ def select_options():
             invalid_option()
     except:
         invalid_option()
-
-def close_app():
-    os.system('clear')
-    print('Encerrando o programa')
 
 def main():
     os.system('clear')
